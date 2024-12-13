4b0e0d1beeb5da564bb0c059ebf041ff8aa520e7
Ikbendennis <58038071+Ikbendennis@users.noreply.github.com>
2024-12-13 14:54:21 +0100

    docker fix

--- src/main/java/com/babymonitoring/simulationplayer/controllers/MessageController.java
+++ src/main/java/com/babymonitoring/simulationplayer/controllers/MessageController.java
@@ -8,11 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.HtmlUtils;
 
 import java.util.concurrent.ExecutionException;
 
 @Controller
+@RequestMapping("/matlab")
 public class MessageController {
 
     private SimpMessagingTemplate template;
